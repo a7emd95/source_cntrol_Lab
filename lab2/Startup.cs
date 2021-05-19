75cf24b352d302852c3493568f7e162468174e17
ahmed abdelaal <ahmed.abdelaal.aly@gmail.com>
2021-05-19 11:39:15 +0200

    Frist commit

--- lab2/Startup.cs
+++ lab2/Startup.cs
@@ -32,7 +32,7 @@ namespace lab2
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World Frist Time Form Master !");
                 });
             });
         }
